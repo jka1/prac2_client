5c77e5a83b3bef03af15f344b031878a95158997
joelk <joelk@DESKTOP-EVAEAV1.home>
2020-05-14 19:01:51 +0200

    Comprovacions finals, agafar els valors del form al servlet

--- src/servlets/SvlAltaLocal.java
+++ src/servlets/SvlAltaLocal.java
@@ -40,6 +40,13 @@ public class SvlAltaLocal extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(request.getParameter("nomlocal"));
+		System.out.println(request.getParameter("coditipolocal"));
+		System.out.println(request.getParameter("codicarrer"));
+		System.out.println(request.getParameter("nomcarrer"));
+		System.out.println(request.getParameter("nomvia"));
+		System.out.println(request.getParameter("numero"));
+		System.out.println(request.getParameter("observacions"));
 		doGet(request, response);
 	}
 
